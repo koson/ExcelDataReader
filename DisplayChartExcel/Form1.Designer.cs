a6f2cd6c7e94bf98aa54be19a04c91b7334327ee
koson trachu <koson.trachu@gmail.com>
2018-10-03 03:13:55 +0700

    add controls,

--- DisplayChartExcel/Form1.Designer.cs
+++ DisplayChartExcel/Form1.Designer.cs
@@ -31,6 +31,14 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.firstRowNamesCheckBox = new System.Windows.Forms.CheckBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.sheetCombo = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // textBox1
@@ -46,7 +54,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Text = "Select";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -54,15 +62,76 @@
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(468, 38);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "process";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // firstRowNamesCheckBox
+            // 
+            this.firstRowNamesCheckBox.AutoSize = true;
+            this.firstRowNamesCheckBox.Location = new System.Drawing.Point(276, 44);
+            this.firstRowNamesCheckBox.Name = "firstRowNamesCheckBox";
+            this.firstRowNamesCheckBox.Size = new System.Drawing.Size(176, 17);
+            this.firstRowNamesCheckBox.TabIndex = 3;
+            this.firstRowNamesCheckBox.Text = "first row contains column names";
+            this.firstRowNamesCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 5;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // sheetCombo
+            // 
+            this.sheetCombo.FormattingEnabled = true;
+            this.sheetCombo.Location = new System.Drawing.Point(199, 82);
+            this.sheetCombo.Name = "sheetCombo";
+            this.sheetCombo.Size = new System.Drawing.Size(121, 21);
+            this.sheetCombo.TabIndex = 6;
+            this.sheetCombo.SelectedIndexChanged += new System.EventHandler(this.sheetCombo_SelectedIndexChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(64, 134);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(388, 198);
+            this.dataGridView1.TabIndex = 7;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.sheetCombo);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.firstRowNamesCheckBox);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -73,6 +142,12 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox firstRowNamesCheckBox;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ComboBox sheetCombo;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- DisplayChartExcel/Form1.cs
+++ DisplayChartExcel/Form1.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ExcelDataReader;
+using System.Diagnostics;
 
 namespace DisplayChartExcel
 {
     public partial class Form1 : Form
     {
+        private DataSet ds;
         public Form1()
         {
             InitializeComponent();
@@ -24,6 +28,84 @@ namespace DisplayChartExcel
             {
                 textBox1.Text = openFileDialog1.FileName;
             }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var extension = Path.GetExtension(textBox1.Text).ToLower();
+            using (var stream = new FileStream(textBox1.Text, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            {
+                var sw = new Stopwatch();
+                sw.Start();
+                IExcelDataReader reader = null;
+                if (extension == ".xls")
+                {
+                    reader = ExcelReaderFactory.CreateBinaryReader(stream);
+                }
+                else if (extension == ".xlsx")
+                {
+                    reader = ExcelReaderFactory.CreateOpenXmlReader(stream);
+                }
+                else if (extension == ".csv")
+                {
+                    reader = ExcelReaderFactory.CreateCsvReader(stream);
+                }
+
+                if (reader == null)
+                    return;
+
+                var openTiming = sw.ElapsedMilliseconds;
+                // reader.IsFirstRowAsColumnNames = firstRowNamesCheckBox.Checked;
+                using (reader)
+                {
+                    ds = reader.AsDataSet(new ExcelDataSetConfiguration()
+                    {
+                        UseColumnDataType = false,
+                        ConfigureDataTable = (tableReader) => new ExcelDataTableConfiguration()
+                        {
+                            UseHeaderRow = firstRowNamesCheckBox.Checked
+                        }
+                    });
+                }
+
+                toolStripStatusLabel1.Text = "Elapsed: " + sw.ElapsedMilliseconds.ToString() + " ms (" + openTiming.ToString() + " ms to open)";
+
+                var tablenames = GetTablenames(ds.Tables);
+                sheetCombo.DataSource = tablenames;
+
+                if (tablenames.Count > 0)
+                    sheetCombo.SelectedIndex = 0;
+
+                // dataGridView1.DataSource = ds;
+                // dataGridView1.DataMember
+            }
+        }
+
+        private static IList<string> GetTablenames(DataTableCollection tables)
+        {
+            var tableList = new List<string>();
+            foreach (var table in tables)
+            {
+                tableList.Add(table.ToString());
+            }
+
+            return tableList;
+        }
+
+        private void sheetCombo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SelectTable();
+        }
+
+        private void SelectTable()
+        {
+            var tablename = sheetCombo.SelectedItem.ToString();
+
+            dataGridView1.AutoGenerateColumns = true;
+            dataGridView1.DataSource = ds; // dataset
+            dataGridView1.DataMember = tablename;
+
+            // GetValues(ds, tablename);
         }
     }
 }
